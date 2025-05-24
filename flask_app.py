7736c937145320828ad334fb386f3cfad0e633b8
admin <freddy.qin@gmail.com>
2025-05-24 16:52:18 +0800

    Add env read logic and local docker-compose.yml

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,18 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+db = os.getenv("DB")
+
+print(f"Env printing: host->{host},user->{user},password->{password},db->{db}")
+
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
-
